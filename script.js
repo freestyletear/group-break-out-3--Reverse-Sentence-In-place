70d249b806264017039c4aea61841a5aec2e69f5
freestyletear <freestyletear@gmail.com>
2021-07-15 14:51:41 +0300

    modify script

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const A = ["this", "is", "good"];
-console.log("A:", A);
-// expected output: "A:" Array ["this", "is", "good"]
+let reverseSentence = (arr1) => {
+  arr1 = arr1.join(",").split(" ").reverse();
+  let reversedArray = arr1.join().split(",");
+  reversedArray.pop();
+  reversedArray.shift();
+  return console.log(reversedArray);
+};
 
-const reversed = A.reverse();
-console.log("reversed:", reversed);
-// expected output: "reversed:" Array ["this", "is", "good"]
+reverseSentence(["a", "b", "c", " ", "d", "e"]);
+reverseSentence(["t", "h", "i", "s", " ", "i", "s", " ", "g", "o", "o", "d"]);
+b = ["i", "s", " ", "t", "h", "i", "s", " ", "g", "o", "o", "d"];
+reverseSentence(b);
 
-// Careful: reverse is destructive -- it changes the original array.
-console.log("A:", A);
-// expected output: "A:" Array ["this", "is", "good"]
+// const A = ['this', 'is', 'good'];
+// console.log("A:", A);
+// // expected output: "A:" Array ['this', 'is', 'good']
+
+// const reversed = A.reverse();
+// console.log("reversed:", reversed);
+// // expected output: "reversed:" Array ['this', 'is', 'good']
+
+// // Careful: reverse is destructive -- it changes the original array.
+// console.log("A:", A);
+// // expected output: "A:" Array ['good', 'is', 'this']
