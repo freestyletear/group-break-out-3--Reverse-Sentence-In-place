6d4d624e8ae836cf067193f6bb8fa303099e9185
freestyletear <freestyletear@gmail.com>
2021-07-15 15:19:43 +0300

    add code

--- script.js
+++ script.js
@@ -11,6 +11,25 @@ reverseSentence(["t", "h", "i", "s", " ", "i", "s", " ", "g", "o", "o", "d"]);
 let b = ["i", "s", " ", "t", "h", "i", "s", " ", "g", "o", "o", "d"];
 reverseSentence(b);
 
+
+
+
+// or;
+
+
+
+// var sentence = ["t", "h", "i", "s", " ", "i", "s", " ", "g", "o", "o", "d"];
+// var reversedS = reverse(sentence).split(",");
+// function reverse(s) {
+//   return s.toString().split(" ").reverse().join(" ");
+// }
+// console.log(reversedS);
+
+
+
+// or
+
+
 // const A = ['this', 'is', 'good'];
 // console.log("A:", A);
 // // expected output: "A:" Array ['this', 'is', 'good']
