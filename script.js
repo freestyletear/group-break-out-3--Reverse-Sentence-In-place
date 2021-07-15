7ac8faf3e9013753ea35b1a270ddd524c3a8d94c
freestyletear <freestyletear@gmail.com>
2021-07-15 15:34:28 +0300

    add code

--- script.js
+++ script.js
@@ -32,5 +32,14 @@ console.log(reversedS);
 // or
 
 
-// 
+const A = ['this', 'is', 'good'];
+console.log("A:", A);
+// expected output: "A:" Array ['this', 'is', 'good']
 
+const reversed = A.reverse();
+console.log("reversed:", reversed);
+// expected output: "reversed:" Array ['this', 'is', 'good']
+
+// Careful: reverse is destructive -- it changes the original array.
+console.log("A:", A);
+// expected output: "A:" Array ['good', 'is', 'this']
