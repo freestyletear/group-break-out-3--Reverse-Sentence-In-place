3c11a296d52c66d40ab3fc90b6d67f1645492b06
freestyletear <freestyletear@gmail.com>
2021-07-15 15:27:31 +0300

    modify code

--- script.js
+++ script.js
@@ -18,26 +18,19 @@ reverseSentence(b);
 
 
 
-// var sentence = ["t", "h", "i", "s", " ", "i", "s", " ", "g", "o", "o", "d"];
-// var reversedS = reverse(sentence).split(",");
-// function reverse(s) {
-//   return s.toString().split(" ").reverse().join(" ");
-// }
-// console.log(reversedS);
+var sentence = ["t", "h", "i", "s", " ", "i", "s", " ", "g", "o", "o", "d"];
+var reversedS = reverse(sentence).split(",");
+function reverse(s) {
+  return s.toString().split(" ").reverse().join(" ");
+}
+reversedS.shift();
+reversedS.pop();
+console.log(reversedS);
 
 
 
 // or
 
 
-// const A = ['this', 'is', 'good'];
-// console.log("A:", A);
-// // expected output: "A:" Array ['this', 'is', 'good']
+// 
 
-// const reversed = A.reverse();
-// console.log("reversed:", reversed);
-// // expected output: "reversed:" Array ['this', 'is', 'good']
-
-// // Careful: reverse is destructive -- it changes the original array.
-// console.log("A:", A);
-// // expected output: "A:" Array ['good', 'is', 'this']
